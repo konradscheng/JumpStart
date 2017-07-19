ac6d591c837f6cc4d5308d39d9052832c670d53e
konradscheng <konradscheng@gmail.com>
2017-07-19 01:03:12 -1000

    Finished with navbar animations.

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,5 +1,3 @@
-alert("Connected Hi");
-
 $(document).ready(function(){
   $('.carousel').slick({
     slidesToScroll: 1,
@@ -9,5 +7,18 @@ $(document).ready(function(){
     draggable: false,
     pauseOnFocus: false,
     pauseOnHover: false
+  });
+
+  var scroll = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - scroll > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      scroll = scrollTop;
+    } else if (scroll - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      scroll = scrollTop;
+    }
   });
 });
